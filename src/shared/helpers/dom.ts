28f4c309d5108e3937720b77bc5bb7c683121d9e
Lnola <nola.luka@gmail.com>
2023-11-14 22:33:54 +0100

    :sparkles: Implement setButtonIsDisabled

--- src/shared/helpers/dom.ts
+++ src/shared/helpers/dom.ts
@@ -3,6 +3,11 @@ class DomHelpers {
     const button = document.getElementById(buttonId) as HTMLButtonElement;
     button.onclick = method;
   }
+
+  static setButtonIsDisabled(buttonId: string, isDisabled: boolean) {
+    const button = document.getElementById(buttonId) as HTMLButtonElement;
+    button.disabled = isDisabled;
+  }
 }
 
 export default DomHelpers;
