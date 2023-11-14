dd747e57439b40415a7efb1a46aaaff43a20b935
Lnola <nola.luka@gmail.com>
2023-11-14 22:38:22 +0100

    :sparkles: Implement setElementInnerHtml helper method

--- src/shared/helpers/dom.ts
+++ src/shared/helpers/dom.ts
@@ -8,6 +8,10 @@ class DomHelpers {
     const button = document.getElementById(buttonId) as HTMLButtonElement;
     button.disabled = isDisabled;
   }
+
+  static setElementInnerHtml(elementId: string, innerHtml: string) {
+    document.getElementById(elementId)!.innerHTML = innerHtml;
+  }
 }
 
 export default DomHelpers;
