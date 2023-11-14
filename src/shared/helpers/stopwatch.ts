02810607fbcb66c56447217feecaf9bdd6f8bcf9
Lnola <nola.luka@gmail.com>
2023-11-14 21:55:51 +0100

    :truck: Sort methods

--- src/shared/helpers/stopwatch.ts
+++ src/shared/helpers/stopwatch.ts
@@ -24,14 +24,14 @@ class Stopwatch {
     return new Date().getTime();
   }
 
+  private get displayTime() {
+    return `Time: ${this.elapsedTime.toString()}`;
+  }
+
   private update() {
     const elapsedMiliseconds = this.now - this.startTime;
     this.elapsedTime.setTime(elapsedMiliseconds);
     document.getElementById('stopwatch')!.innerHTML = this.displayTime;
-  }
-
-  private get displayTime() {
-    return `Time: ${this.elapsedTime.toString()}`;
   }
 }
 
