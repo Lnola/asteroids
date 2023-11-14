be7335549056be6a211f8c5595e2878ec21fdf04
Lnola <nola.luka@gmail.com>
2023-11-14 23:18:47 +0100

    :technologist: Create a movement type enum

--- src/shared/models/movement.ts
+++ src/shared/models/movement.ts
@@ -1,1 +1,8 @@
-export type MovementType = 'linear' | 'rotation';
+import { ValuesType } from 'utility-types';
+
+export const MovementType = {
+  LINEAR: 'LINEAR',
+  ROTATION: 'ROTATION',
+} as const;
+
+export type MovementType = ValuesType<typeof MovementType>;
