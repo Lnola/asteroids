3cc550302421d55bcf2891bbf204fbb46f81a5ca
Lnola <nola.luka@gmail.com>
2023-11-15 01:00:24 +0100

    :recycle: Extract getBestTime into a separate function

--- src/shared/helpers/best-time.ts
+++ src/shared/helpers/best-time.ts
@@ -2,21 +2,22 @@ import { DomHelpers, Stopwatch, Store, Time } from '.';
 import { BEST_TIME_ID } from '@/shared/models/dom';
 
 class BestTimeHelpers {
-  static updateBestTime(bestTimeStore: Store, stopwatch: Stopwatch) {
-    const bestTime = new Time(
-      bestTimeStore.value.minutes,
-      bestTimeStore.value.seconds,
+  static getBestTime(bestTimeStore: Store) {
+    return new Time(
+      bestTimeStore.value?.minutes ?? 0,
+      bestTimeStore.value?.seconds ?? 0,
     );
+  }
+
+  static updateBestTime(bestTimeStore: Store, stopwatch: Stopwatch) {
+    const bestTime = this.getBestTime(bestTimeStore);
     const currentTime = stopwatch.elapsedTime;
     if (!(bestTime && currentTime.isGreaterThan(bestTime))) return;
     bestTimeStore.setValue(stopwatch.elapsedTime);
   }
 
   static setBestTimeLabel(bestTimeStore: Store) {
-    const bestTime = new Time(
-      bestTimeStore.value?.minutes ?? 0,
-      bestTimeStore.value?.seconds ?? 0,
-    );
+    const bestTime = this.getBestTime(bestTimeStore);
     const displayBestTime = `Best time: ${bestTime.toString()}`;
     DomHelpers.setElementInnerHtml(BEST_TIME_ID, displayBestTime);
   }
